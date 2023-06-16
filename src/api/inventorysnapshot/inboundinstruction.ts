10b2fdc3f5618972ef373e9d4a466d152fc74210
Janekei <1210259220@qq.com>
2023-06-16 16:31:51 +0800

    添加获取运单信息、柜信息接口

--- src/api/inventorysnapshot/inboundinstruction.ts
+++ src/api/inventorysnapshot/inboundinstruction.ts
@@ -1,7 +1,12 @@
 import request from '@/config/axios'
 
 
-// 获取仓库信息 ----传入参数id
+// 获取入库指令信息 ----传入参数id
 export const getInboundList = async (params) => {
     return await request.get({ url: '/jinkotms-moduule-core-biz/gsc-wh-inbound/page', params })
+}
+
+// 获取运单信息、柜信息 ----传入参数id
+export const getListItemDetail = async (params) => {
+    return await request.get({ url: '/jinkotms-moduule-core-biz/gsc-wh-inbound/getInformation', params })
 }
