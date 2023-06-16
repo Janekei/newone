719a232639010a5d6f1c47c5355e19669e0cbbe6
Janekei <1210259220@qq.com>
2023-06-16 16:31:05 +0800

    修改运单信息页面样式，使用ElDescriptions组件

---
+++ src/views/InventorySnapshot/InboundInstruction/components/DescriptionInboundList.vue
@@ -0,0 +1,34 @@
+<template>
+    <el-descriptions :column="5" border>
+        <el-descriptions-item label="Username" label-align="left" align="center" label-class-name="my-label"
+            class-name="my-content">kooriookami</el-descriptions-item>
+        <el-descriptions-item label="Username" label-align="left" align="center" label-class-name="my-label"
+            class-name="my-content">kooriookami</el-descriptions-item>
+        <el-descriptions-item label="Username" label-align="left" align="center" label-class-name="my-label"
+            class-name="my-content">kooriookami</el-descriptions-item>
+        <el-descriptions-item label="Username" label-align="rleft" align="center" label-class-name="my-label"
+            class-name="my-content">kooriookami</el-descriptions-item>
+        <el-descriptions-item label="Username" width="180" label-align="rleft" align="center" label-class-name="my-label"
+            class-name="my-content">kooriookam11111111111i1111111111</el-descriptions-item>
+    </el-descriptions>
+</template>
+
+<script lang="ts" setup>
+import { ElDescriptions, ElDescriptionsItem } from 'element-plus'
+// const props = defineProps({
+//     descList: {
+//         type: Array as any,
+//         default: () => []
+//     }
+// })
+
+// 过滤当前行的数据
+// const needDescData = props.descList.filters(() => {
+// })
+</script >
+<style lang="scss" scoped>
+::v-deep .is-bordered-label {
+    background-color: #e8e8e8 !important;
+    font-weight: 700 !important;
+}
+</style>
--- src/views/InventorySnapshot/InboundInstruction/components/TableWayBill.vue
+++ src/views/InventorySnapshot/InboundInstruction/components/TableWayBill.vue
@@ -1,52 +1,74 @@
 <template>
     <div class="box">
-        <el-row class="title">
-            <el-col :span="24">
-                <div class="ep-bg-purple-dark">{{ props.data.title }}</div>
-            </el-col>
-        </el-row>
         <div class="content">
-            <el-row class="el_row" :gutter="20" v-for="(item, index) in list" :key="index">
-                <el-col class="el_col" :span="6">
-                    <div class="ep-bg-purple">{{ item[0].title }}</div>
-                </el-col>
-                <el-col class="el_col" :span="6">
-                    <div class="ep-bg-purple">{{ item[0].value }}</div>
-                </el-col>
-                <el-col class="el_col" :span="6">
-                    <div class="ep-bg-purple">{{ item[1].title }}</div>
-                </el-col>
-                <el-col class="el_col" :span="6">
-                    <div class="ep-bg-purple">{{ item[1].value }}</div>
-                </el-col>
-            </el-row>
+            <el-descriptions class="elDescriptions" :title="res1.title" align="center" :column="2" border>
+                <el-descriptions-item v-for="(item, index) in res1.list" :key="index + 'k'" :label="item.title"
+                    label-align="left" align="center" label-class-name="my-label" class-name="my-content" width="180">{{
+                        item.value
+                    }}</el-descriptions-item>
+            </el-descriptions>
+            <el-descriptions class="elDescriptions" :title="res2.title" :column="2" border>
+                <el-descriptions-item v-for="(item, index) in res2.list" :key="index + 'k'" :label="item.title"
+                    label-align="left" align="center" label-class-name="my-label" class-name="my-content">{{ item.value
+                    }}</el-descriptions-item>
+            </el-descriptions>
+            <el-descriptions class="elDescriptions" :title="res3.title" :column="2" border>
+                <el-descriptions-item v-for="(item, index) in res3.list" :key="index + 'k'" :label="item.title"
+                    label-align="left" align="center" label-class-name="my-label" class-name="my-content">{{ item.value
+                    }}</el-descriptions-item>
+            </el-descriptions>
         </div>
     </div>
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue'
-import { ElRow, ElCol } from 'element-plus'
-const props = defineProps({
-    data: {
-        type: Array as any,
-        default: () => { }
-    }
+import { reactive } from 'vue'
+import { ElDescriptions, ElDescriptionsItem } from 'element-plus'
+const res1 = reactive({
+    title: '任务明细',
+    list: [
+        { title: '任务单号', value: 'xxxxx' },
+        { title: '发货方', value: 'xxxxx' },
+        { title: 'SAP ID', value: 'xxxxx' },
+        { title: '收货方', value: 'xxxxx' },
+        { title: '业务类型', value: 'xxxxx' },
+        { title: '承运商', value: 'xxxxx' },
+        { title: '运输方式', value: 'xxxxx' },
+        { title: '贸易方式', value: 'xxxxx' },
+        { title: '起运港', value: 'xxxxx' },
+        { title: '目的港', value: 'xxxxx' },
+        { title: '提货时间', value: 'xxxxx' },
+        { title: '需求方', value: 'xxxxx' },
+    ]
 })
-// 过滤数据
-const list = computed(() => {
-    let data: any = []
-    for (let i = 0; i < props.data.list.length; i = i + 2) {
-        data.push([
-            props.data.list[i],
-            props.data.list[i + 1]
-        ])
-    }
-    console.log(data, 'data')
-    return data
+const res2 = reactive({
+    title: '货品明细',
+    list: [
+        { title: '提单号', value: 'xxxxx' },
+        { title: '发票号', value: 'xxxxx' },
+        { title: '合同号', value: 'xxxxx' },
+        { title: '集装箱数', value: 'xxxxx' },
+        { title: '货值', value: 'xxxxx' },
+        { title: '瓦数', value: 'xxxxx' },
+        { title: '功率档位', value: 'xxxxx' },
+        { title: '托盘数量', value: 'xxxxx' },
+        { title: '片数', value: 'xxxxx' }
+    ]
 })
-onBeforeMount(() => {
-    console.log(props.data)
+const res3 = reactive({
+    title: '时间明细',
+    list: [
+        { title: '生成入库指令时间', value: 'xxxxx' },
+        { title: 'ETD日期', value: 'xxxxx' },
+        { title: 'ETD New 日期', value: 'xxxxx' },
+        { title: 'ATD日期', value: 'xxxxx' },
+        { title: 'ETA日期', value: 'xxxxx' },
+        { title: 'ETA New 日期', value: 'xxxxx' },
+        { title: 'ATA日期', value: 'xxxxx' },
+        { title: '预计入库时间', value: 'xxxxx' },
+        { title: '港口提货时间', value: 'xxxxx' },
+        { title: '清关完成时间', value: 'xxxxx' }
+    ]
 })
 </script>
 <style lang="scss" scoped>
@@ -65,11 +87,26 @@ onBeforeMount(() => {
 }
 
 .content {
-    border: .125rem solid #bebebe;
+    // border: .125rem solid #bebebe;
     border-top: none;
     border-bottom: none;
     overflow: hidden;
+    margin-bottom: .625rem;
+
+    .elDescriptions {
+        margin-bottom: .625rem;
+    }
+
 }
+
+::v-deep .el-descriptions__title {
+    width: 100%;
+    background-color: #d1e8ff;
+    text-align: center;
+    padding: .3125rem 0;
+    border-radius: 1.25rem;
+}
+
 
 .el_col {
     border: 0.0625rem solid #bebebe;
