de6712aeefb06bd5e946dc60fe43111db371cd4c
李武钢 <2252422742@qq.com>
2023-06-21 11:57:55 +0800

    去掉无用变量

--- src/views/InventorySnapshot/OutboundInstruction/components/TopTabs.vue
+++ src/views/InventorySnapshot/OutboundInstruction/components/TopTabs.vue
@@ -1,12 +1,11 @@
 <template>
-    <el-tabs v-model="activeName" type="card" class="demo-tabs tab" @tab-click="handleClick">
-        <el-tab-pane v-for="(item, index) in tabList" :label="item.lable" :key="item.title" :name="item.name"
-            v-loading="loading">
-            <slot :name="item.name" v-if="activeName === item.name">
-                {{ item.name }}
-            </slot>
-        </el-tab-pane>
-    </el-tabs>
+ <el-tabs v-model="activeName" type="card" class="demo-tabs tab" @tab-click="handleClick">
+  <el-tab-pane v-for="item in tabList" :label="item.lable" :key="item.title" :name="item.name" v-loading="loading">
+   <slot :name="item.name" v-if="activeName === item.name">
+    {{ item.name }}
+   </slot>
+  </el-tab-pane>
+ </el-tabs>
 </template>
 
 <script lang="ts" setup>
@@ -15,10 +14,10 @@ import { ElTabPane, ElTabs } from 'element-plus'
 
 
 const props = defineProps({
-    tabList: {
-        type: Array as any,
-        default: () => []
-    }
+ tabList: {
+  type: Array as any,
+  default: () => []
+ }
 })
 
 const loading = ref(false)
@@ -27,26 +26,26 @@ let activeName = ref(props.tabList[0].name)
 
 // 过滤tabList数据
 const tabList = computed(() => {
-    let data: any = []
-    if (props.tabList[0].hasOwnProperty('number')) {
-        props.tabList.forEach(item => {
-            data.push({
-                title: item.title,
-                lable: item.title + '（' + item.number + '）',
-                name: item.name,
-                number: item.number
-            })
-        });
-        return data;
-    }
-    props.tabList.forEach(item => {
-        data.push({
-            title: item.title,
-            lable: item.title,
-            name: item.name
-        })
-    });
-    return data;
+ let data: any = []
+ if (props.tabList[0].hasOwnProperty('number')) {
+  props.tabList.forEach(item => {
+   data.push({
+    title: item.title,
+    lable: item.title + '（' + item.number + '）',
+    name: item.name,
+    number: item.number
+   })
+  });
+  return data;
+ }
+ props.tabList.forEach(item => {
+  data.push({
+   title: item.title,
+   lable: item.title,
+   name: item.name
+  })
+ });
+ return data;
 })
 
 
@@ -55,7 +54,7 @@ const tabList = computed(() => {
 
 // 选中的tab触发
 const handleClick = (tab) => {
-    console.log(tab)
+ console.log(tab)
 }
 
 
@@ -64,31 +63,31 @@ const handleClick = (tab) => {
 </script>
 <style lang="scss" scoped>
 .demo-tabs>.el-tabs__content {
-    padding: 32px;
-    color: #6b778c;
-    font-size: 1.75rem;
+ padding: 32px;
+ color: #6b778c;
+ font-size: 1.75rem;
 
-    background-color: #6b778c;
+ background-color: #6b778c;
 }
 
 ::v-deep .el-tabs__nav>.el-tabs__item {
-    color: #409EFF;
-    background-color: #fff;
-    font-family: "PingFang SC";
-    border: none;
-    border-top: .125rem solid #409EFF !important;
+ color: #409EFF;
+ background-color: #fff;
+ font-family: "PingFang SC";
+ border: none;
+ border-top: .125rem solid #409EFF !important;
 }
 
 ::v-deep .el-tabs__header .el-tabs__nav {
-    border: none;
+ border: none;
 }
 
 ::v-deep .el-tabs__header {
-    border: none !important;
+ border: none !important;
 }
 
 ::v-deep .el-tabs__nav>.is-active {
-    border: 0.0625rem solid #409EFF !important;
-    border-top: .375rem solid #409EFF !important;
+ border: 0.0625rem solid #409EFF !important;
+ border-top: .375rem solid #409EFF !important;
 }
 </style>
