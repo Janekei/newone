047e32b4c6b6a00d2b4c2a90a114dd136dcdd2a1
李武钢 <2252422742@qq.com>
2023-06-26 14:25:59 +0800

    1

--- src/components/FormK/Components/MyInputTable.vue
+++ src/components/FormK/Components/MyInputTable.vue
@@ -1,20 +1,20 @@
 <template>
-  <ElInput v-model="curValue" :placeholder="placeholder" @input="change" :clearable="true" @keyup.enter="enterFn"
-    style="width: 100%">
-    <template #append>
-      <ElButton :icon="Search" style="margin: 0 -23px" ref="buttonRef" v-click-outside="onClickOutside" />
-    </template>
+ <ElInput v-model="curValue" :placeholder="placeholder" @input="change" :clearable="true" @keyup.enter="enterFn"
+  style="width: 100%" @blur="onBlur">
+  <template #append>
+   <ElButton :icon="Search" style="margin: 0 -23px" ref="buttonRef" v-click-outside="onClickOutside" />
+  </template>
+ </ElInput>
+ <ElPopover ref="popoverRef" :virtual-ref="buttonRef" trigger="click" virtual-triggering width="auto"
+  v-model:visible="showPopoverRef">
+  <ElInput v-model="searchKey" :clearable="true">
+   <template #append>
+    <ElButton :icon="Search" style="margin: 0 -23px" @click="searchTable" />
+   </template>
   </ElInput>
-  <ElPopover ref="popoverRef" :virtual-ref="buttonRef" trigger="click" virtual-triggering width="auto"
-    v-model:visible="showPopoverRef">
-    <ElInput v-model="searchKey" :clearable="true">
-      <template #append>
-        <ElButton :icon="Search" style="margin: 0 -23px" @click="searchTable" />
-      </template>
-    </ElInput>
-    <TableK :tableOption="tableConfig.tableOption" size="small" :firstPages="5" :showCheckBox="false" pageSmall
-      ref="TableKRef" @clickThisColumn="clickThisColumn" :url="tableConfig.url" :params="params" />
-  </ElPopover>
+  <TableK :tableOption="tableConfig.tableOption" size="small" :firstPages="5" :showCheckBox="false" pageSmall
+   ref="TableKRef" @clickThisColumn="clickThisColumn" :url="tableConfig.url" :params="params" />
+ </ElPopover>
 </template>
 
 <script lang="ts" setup>
@@ -25,22 +25,22 @@ import TableK from '@/components/TableK/index.vue'
 
 const emits: any = defineEmits(['onChange'])
 const props = defineProps({
-  placeholder: {
-    type: String,
-    default: '请输入'
-  },
-  modelValue: {
-    type: [String, Number],
-    default: ''
-  },
-  valueKey: {
-    type: String,
-    default: ''
-  },
-  tableConfig: {
-    type: Object as any,
-    default: () => ({})
-  }
+ placeholder: {
+  type: String,
+  default: '请输入'
+ },
+ modelValue: {
+  type: [String, Number],
+  default: ''
+ },
+ valueKey: {
+  type: String,
+  default: ''
+ },
+ tableConfig: {
+  type: Object as any,
+  default: () => ({})
+ }
 })
 
 console.log(props.tableConfig, 46);
@@ -53,45 +53,48 @@ const buttonRef = ref()
 const popoverRef = ref()
 // 按钮点击事件
 const onClickOutside = () => {
-  console.log(props.tableConfig.params, 'tableconfig')
-  unref(popoverRef).popperRef?.delayHide?.()
+ unref(popoverRef).popperRef?.delayHide?.()
 }
 
 // 表格请求参数
 const searchKey = ref()
 const params = computed(() => {
-  return {
-    ...props.tableConfig.params,
-    searchKey: searchKey.value
-  }
+ return {
+  ...props.tableConfig.params,
+  searchKey: searchKey.value
+ }
 })
 
 // 回车事件
 const showPopoverRef = ref(false)
 const enterFn = () => {
-  searchKey.value = curValue.value
-  showPopoverRef.value = true
+ searchKey.value = curValue.value
+ showPopoverRef.value = true
 }
 
 watch(() => props.modelValue, () => {
-  curValue.value = props.modelValue
+ curValue.value = props.modelValue
 }, { immediate: true })
 
 const change = (value: any) => {
-  emits('update:modelValue', value)
-  emits('onChange', value)
+ emits('update:modelValue', value)
+ emits('onChange', value)
 }
 
 // table行点击事件
 const clickThisColumn = (row: any) => {
-  console.log(row, props.valueKey, 'row')
-  change(row[props.valueKey])
+ change(row[props.valueKey])
 }
 
 // 搜索可选项
 const TableKRef = ref()
 const searchTable = () => {
-  TableKRef.value.refresh()
+ TableKRef.value.refresh()
+}
+
+// 外层input失去焦点
+const onBlur = () => {
+ change('')
 }
 </script>
 
