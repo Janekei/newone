f30c0b92adfa8e384aebb8717142594ef1459c15
Janekei <1210259220@qq.com>
2023-06-27 11:57:14 +0800

    标准时间规则表页面开发，完成查询功能

--- src/components/TableK/index.vue
+++ src/components/TableK/index.vue
@@ -1,49 +1,49 @@
 <template>
- <div class="table-box">
-  <RightClickCard style="background-color: #fff;">
-   <slot name="buttons" :selectRow="selectAll"></slot>
-   <!-- tableData.slice((page - 1) * limit, page * limit) -->
-   <div class="table-header">
-    <ElPopover placement="bottom" title="列排序" trigger="click">
-     <template #reference>
-      <ElIcon class="pointer" style="font-size: 20px;" title="列排序">
-       <DCaret />
-      </ElIcon>
-     </template>
-     <template #default>
-      <ColumnSort :tableOption="tableOption" />
-     </template>
-    </ElPopover>
-   </div>
-   <ElTable :data="tableData" style="width: 100%" border v-loading="loading" element-loading-text="数据加载中" :size="size"
-    ref="elTable" @selection-change="handleSelectionChange" @row-click="rowClick" @row-dblclick="rowDblclick"
-    @row-contextmenu="rowContextmenu" :cell-style="{ textAlign: 'center' }"
-    :header-cell-style="{ 'text-align': 'center' }">
-    <ElTableColumn type="selection" width="55" v-if="showCheckBox" />
-    <ElTableColumn label="序号" type="index" width="100" align="center" v-if="showIndex" />
-    <ElTableColumn :prop="item.prop" :label="item.label" :width="item.width" v-for="(item, index) in tableOption"
-     :key="index + 'a'">
-     <template #default="{ row }" v-if="item.slotName">
-      <slot :name="item.slotName" :row="{ row }">{{ index }}</slot>
-     </template>
-    </ElTableColumn>
-    <ElTableColumn type="expand" v-if="showExpand">
-     <template #default="{ row }">
-      <slot name="expand" :expandRow="row"></slot>
-     </template>
-    </ElTableColumn>
-    <ElTableColumn fixed="right" label="操作" width="150" v-if="showFixedOperation">
-     <template #default="{ row }">
-      <slot name="operation" :operateRow="row"></slot>
-     </template>
-    </ElTableColumn>
-   </ElTable>
-   <div class="pagination">
-    <PaginationK @handleCurrentChange="handleCurrentChange" @handleSizeChange="handleSizeChange" :disabled="disabledPage"
-     :firstPages="firstPages" :total="total" ref="pagination" :small="pageSmall" />
-   </div>
-  </RightClickCard>
- </div>
+  <div class="table-box">
+    <RightClickCard style="background-color: #fff;">
+      <slot name="buttons" :selectRow="selectAll"></slot>
+      <!-- tableData.slice((page - 1) * limit, page * limit) -->
+      <div class="table-header">
+        <ElPopover placement="bottom" title="列排序" trigger="click">
+          <template #reference>
+            <ElIcon class="pointer" style="font-size: 20px;" title="列排序">
+              <DCaret />
+            </ElIcon>
+          </template>
+          <template #default>
+            <ColumnSort :tableOption="tableOption" />
+          </template>
+        </ElPopover>
+      </div>
+      <ElTable :data="tableData" style="width: 100%" border v-loading="loading" element-loading-text="数据加载中" :size="size"
+        ref="elTable" @selection-change="handleSelectionChange" @row-click="rowClick" @row-dblclick="rowDblclick"
+        @row-contextmenu="rowContextmenu" :cell-style="{ textAlign: 'center' }"
+        :header-cell-style="{ 'text-align': 'center' }">
+        <ElTableColumn type="selection" width="55" v-if="showCheckBox" />
+        <ElTableColumn label="序号" type="index" width="100" align="center" v-if="showIndex" />
+        <ElTableColumn :prop="item.prop" :label="item.label" :width="item.width" v-for="(item, index) in tableOption"
+          :key="index + 'a'">
+          <template #default="{ row }" v-if="item.slotName">
+            <slot :name="item.slotName" :row="{ row }">{{ index }}</slot>
+          </template>
+        </ElTableColumn>
+        <ElTableColumn type="expand" v-if="showExpand">
+          <template #default="{ row }">
+            <slot name="expand" :expandRow="row"></slot>
+          </template>
+        </ElTableColumn>
+        <ElTableColumn fixed="right" label="操作" width="150" v-if="showFixedOperation">
+          <template #default="{ row }">
+            <slot name="operation" :operateRow="row"></slot>
+          </template>
+        </ElTableColumn>
+      </ElTable>
+      <div class="pagination">
+        <PaginationK @handleCurrentChange="handleCurrentChange" @handleSizeChange="handleSizeChange"
+          :disabled="disabledPage" :firstPages="firstPages" :total="total" ref="pagination" :small="pageSmall" />
+      </div>
+    </RightClickCard>
+  </div>
 </template>
  
 <script lang="ts" setup>
@@ -119,8 +119,8 @@ const loading = ref(false)
 const loaded = ref(false)
 const total = ref(0)
 const pageParams = reactive({
- pageNo: 1,
- pageSize: props.firstPages
+  pageNo: 1,
+  pageSize: props.firstPages
 })
 const disabledPage = ref(false)
 
@@ -160,30 +160,30 @@ const getData = () => {
 }
 
 onMounted(() => {
- getData()
+  getData()
 })
 
 // 页码改变
 const handleCurrentChange = (value: number) => {
- pageParams.pageNo = value
+  pageParams.pageNo = value
 
- getData()
+  getData()
 }
 
 // 每页条数改变
 const handleSizeChange = (value: number) => {
- pageParams.pageSize = value
- getData()
+  pageParams.pageSize = value
+  getData()
 }
 
 // 刷新方法
 const pagination = ref()
 const refresh = () => {
- pageParams.pageNo = 1
- pageParams.pageSize = props.firstPages
- // 刷新分页器
- pagination.value.refreshView(pageParams.pageSize)
- getData()
+  pageParams.pageNo = 1
+  pageParams.pageSize = props.firstPages
+  // 刷新分页器
+  pagination.value.refreshView(pageParams.pageSize)
+  getData()
 }
 
 // 选中项
@@ -191,45 +191,46 @@ const elTable = ref()
 const selectAll = ref([])
 const emits = defineEmits(['clickThisColumn', 'selectThisColumn'])
 const handleSelectionChange = (rows) => {
- selectAll.value = rows
- emits('selectThisColumn', rows)
+  selectAll.value = rows
+  emits('selectThisColumn', rows)
 }
 const rowClick = (row) => {
- emits('clickThisColumn', row)
- elTable.value!.toggleRowSelection(row, undefined)
+  emits('clickThisColumn', row)
+  elTable.value!.toggleRowSelection(row, undefined)
 }
 const rowDblclick = (row) => {
- elTable.value!.clearSelection()
- elTable.value!.toggleRowSelection(row, undefined)
+  elTable.value!.clearSelection()
+  elTable.value!.toggleRowSelection(row, undefined)
 }
 
 // 鼠标右击面板
 const rowContextmenu = (row, column, event) => {
- console.log(row, column, event);
+  console.log(row, column, event);
 }
 
 defineExpose({
- refresh,
- selectAll
+  refresh,
+  selectAll
 })
 </script>
 
 <style scoped lang="scss">
 .table-box {
- background-color: #fff;
+  background-color: #fff;
 }
 
 .pagination {
- display: flex;
- margin-top: 1rem;
- background-color: #fff;
- padding-bottom: 1rem;
+  display: flex;
+  justify-content: end;
+  margin-top: 1rem;
+  background-color: #fff;
+  padding-bottom: 1rem;
 }
 
 .table-header {
- display: flex;
- justify-content: end;
- padding: 8px 0;
- // padding: 1rem;
+  display: flex;
+  justify-content: end;
+  padding: 8px 0;
+  // padding: 1rem;
 }
 </style>
