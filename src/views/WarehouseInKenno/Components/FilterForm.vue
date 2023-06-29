6765c5dea0e5a2145e9c02882cb751d396272017
李武钢 <2252422742@qq.com>
2023-06-29 18:08:16 +0800

    仓库首页接口联调

--- src/views/WarehouseInKenno/Components/FilterForm.vue
+++ src/views/WarehouseInKenno/Components/FilterForm.vue
@@ -1,10 +1,10 @@
 <template>
- <div class="flex-box-form">
-  <div class="form-title-color filter">筛选</div>
-  <div>
-   <FormK :formOption="formOption" v-model:formState="formData" :marginBottom="false" />
-  </div>
- </div>
+    <div class="flex-box-form">
+        <div class="form-title-color filter">筛选</div>
+        <div>
+            <FormK :formOption="formOption" v-model:formState="formData" :marginBottom="false" />
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -12,61 +12,71 @@ import { ref, reactive } from 'vue'
 import FormK from '@/components/FormK/index.vue'
 
 const formOption = reactive([
- {
-  type: 'select',
-  field: 'region',
-  placeholder: '请选择区域',
-  label: '区域',
-  options: [
-   { label: '全部', value: 0 },
-   { label: '北美', value: 1 },
-   { label: '南亚', value: 2 },
-   { label: '越南', value: 3 },
-   { label: '欧洲', value: 4 }
-  ]
- },
- {
-  type: 'select',
-  field: 'warehouse',
-  placeholder: '请选择仓库',
-  label: '仓库',
-  requestOptions: {
-   url: '/warehouse/list',
-   method: 'get',
-   params: {},
-   handleOptions: (res: any) => {
-    return res.data.map((item: any) => {
-     return {
-      label: item.name,
-      value: item.code
-     }
-    })
-   }
-  }
- },
- {
-  type: 'date',
-  field: 'time',
-  placeholder: '请选择时间',
-  label: '时间'
- },
+    {
+        type: 'inputTable',
+        field: 'areaId',
+        placeholder: '请选择区域',
+        label: '区域',
+        tableConfig: {
+            url: '/jinko/baseWharea/page',
+            method: 'get',
+            params: {},
+            tableOption: [
+                {
+                    prop: 'name',
+                    label: '国家'
+                },
+                {
+                    prop: 'code',
+                    label: 'Code'
+                }
+            ]
+        }
+    },
+    {
+        type: 'inputTable',
+        field: 'warehouseId',
+        placeholder: '请选择仓库',
+        label: '仓库',
+        requestOptions: {
+            url: '/jinko/baseWarehouse/get',
+            method: 'get',
+            params: {
+                id: 1
+            },
+            handleOptions: (res: any) => {
+                return res.data.map((item: any) => {
+                    return {
+                        label: item.name,
+                        value: item.id
+                    }
+                })
+            }
+        }
+    },
+    {
+        type: 'date',
+        field: 'time',
+        placeholder: '请选择时间',
+        label: '时间'
+    },
 ])
 
 const formData = ref({
- userName: '张三'
+    userName: '张三'
 })
 </script>
 
 <style scoped lang="scss">
 .flex-box-form {
- display: flex;
- align-items: center;
- background-color: #d3e4ee;
- padding: .375rem .75rem;
- font-size: 1rem;
+    display: flex;
+    align-items: center;
+    background-color: #d3e4ee;
+    padding: .375rem .75rem;
+    font-size: 1rem;
 }
 
 .filter {
- margin-right: 1rem;
+    margin-right: 1rem;
 }
 </style>
