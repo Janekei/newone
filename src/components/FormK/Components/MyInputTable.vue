3c431379dd9382aadfd9f7abd2b74f5303f12616
Janekei <1210259220@qq.com>
2023-07-03 16:49:06 +0800

    添加清空时触发的事件

--- src/components/FormK/Components/MyInputTable.vue
+++ src/components/FormK/Components/MyInputTable.vue
@@ -1,6 +1,6 @@
 <template>
   <ElInput v-model="curValue" :placeholder="placeholder" @input="change" :clearable="true" @keyup.enter="enterFn"
-    style="width: 100%" @blur="onBlur">
+    style="width: 100%" @blur="onBlur" @clear="clearFn">
     <template #append>
       <ElButton :icon="Search" style="margin: 0 -23px" ref="buttonRef" v-click-outside="onClickOutside" />
     </template>
@@ -19,96 +19,112 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, unref, watch, computed } from 'vue'
-import { ElInput, ElButton, ElPopover, ClickOutside as vClickOutside } from 'element-plus'
-import { Search } from '@element-plus/icons-vue'
-import TableK from '@/components/TableK/index.vue'
+import { ref, unref, watch, computed } from "vue";
+import {
+  ElInput,
+  ElButton,
+  ElPopover,
+  ClickOutside as vClickOutside,
+} from "element-plus";
+import { Search } from "@element-plus/icons-vue";
+import TableK from "@/components/TableK/index.vue";
 
-const emits: any = defineEmits(['onChange', 'getMyInputTableData'])
+const emits: any = defineEmits(["onChange", "getMyInputTableData"]);
 const props = defineProps({
   placeholder: {
     type: String,
-    default: '请输入'
+    default: "请输入",
   },
   modelValue: {
     type: [String, Number],
-    default: ''
+    default: "",
   },
   valueKey: {
     type: String,
-    default: ''
+    default: "",
   },
   tableConfig: {
     type: Object as any,
-    default: () => ({})
+    default: () => ({}),
   },
   setFormData: {
     type: Function,
-    default: null
+    default: null,
   },
-})
+  clearData: {
+    type: Function,
+    default: null,
+  },
+});
 
-
-const curValue = ref()
+const curValue = ref();
 // 按钮
-const buttonRef = ref()
+const buttonRef = ref();
 // ElPopover
-const popoverRef = ref()
+const popoverRef = ref();
 // 按钮点击事件
 const onClickOutside = () => {
-  unref(popoverRef).popperRef?.delayHide?.()
-}
+  unref(popoverRef).popperRef?.delayHide?.();
+};
 
 // 表格请求参数
-const searchKey = ref()
+const searchKey = ref();
 const params = computed(() => {
   return {
     ...props.tableConfig.params,
-    searchKey: searchKey.value
-  }
-})
+    searchKey: searchKey.value,
+  };
+});
 // const getTableData = (tableData) => {
 //   emits('getMyInputTableData', tableData)
 // }
 
-
 // 回车事件
-const showPopoverRef = ref(false)
+const showPopoverRef = ref(false);
 const enterFn = () => {
-  searchKey.value = curValue.value
-  showPopoverRef.value = true
-}
+  searchKey.value = curValue.value;
+  showPopoverRef.value = true;
+};
 
-watch(() => props.modelValue, () => {
-  curValue.value = props.modelValue
-}, { immediate: true })
+// 点击×清空按钮清空输入框
+const clearFn = () => {
+  console.log(111);
+};
+
+watch(
+  () => props.modelValue,
+  () => {
+    curValue.value = props.modelValue;
+  },
+  { immediate: true }
+);
 
 watch(showPopoverRef, (newV) => {
   if (newV) {
-    TableKRef.value.refresh()
+    TableKRef.value.refresh();
   }
-})
+});
 
 const change = (value: any, row?) => {
-  emits('update:modelValue', value)
-  emits('onChange')
-  props.setFormData && props.setFormData(row)
-}
+  emits("update:modelValue", value);
+  emits("onChange");
+  props.setFormData && props.setFormData(row);
+};
 
 const clickThisColumn = (row: any) => {
-  change(row[props.valueKey], row)
-}
+  change(row[props.valueKey], row);
+};
 
 // 搜索可选项
-const TableKRef = ref()
+const TableKRef = ref();
 const searchTable = () => {
-  TableKRef.value.refresh()
-}
+  TableKRef.value.refresh();
+};
 
 // 外层input失去焦点
 const onBlur = () => {
-  change('')
-}
+  change("");
+};
 </script>
 
 <style lang="scss"></style>
