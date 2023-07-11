e4ea308807bcb85a238e028e408bb6b51f2850a6
Janekei <1210259220@qq.com>
2023-07-11 18:49:52 +0800

    出库指令状态修改

--- src/views/InventorySnapshot/OutboundInstruction/index.vue
+++ src/views/InventorySnapshot/OutboundInstruction/index.vue
@@ -17,7 +17,6 @@ const getListTotal = async (status, name) => {
         name,
         total: res
     })
-    console.log(total.value, 'total')
 }
 
 onBeforeMount(() => {
@@ -27,8 +26,8 @@ onBeforeMount(() => {
 })
 
 const tabList = reactive([
-    { title: '全部', name: 'all', number: 30, status: 0 },
-    { title: '待出库', name: 'dck', number: 12, status: 1 },
+    { title: '全部', name: 'all', number: 30, status: 1 },
+    { title: '待出库', name: 'dck', number: 12, status: 0 },
     { title: '待签收', name: 'dqs', number: 12, status: 2 },
 ])
 </script>
