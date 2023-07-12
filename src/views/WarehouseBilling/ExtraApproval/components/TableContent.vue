d503304c7e40093e5acdc9cf4c53e63d3d82b095
Janekei <1210259220@qq.com>
2023-07-12 09:08:05 +0800

    增加额外费用审批页面

---
+++ src/views/WarehouseBilling/ExtraApproval/components/TableContent.vue
@@ -0,0 +1,143 @@
+<template>
+    <div>
+        <TableK url="/jinko/fee/template/page" method="get" ref="tableRef" :params="formData" :firstPages="10"
+            :tableOption="tableOption"  :showCheckBox="false" :showIndex="true">
+            <template #buttons>
+                <SearchContent @click-search="clickSearch" @update:form-state="updateSearchData" @reset-form="resetForm" />
+            </template>
+        </TableK>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, reactive } from 'vue'
+// import { formatTime } from '@/utils'
+import SearchContent from './SearchContent.vue'
+import TableK from '@/components/TableK/index.vue'
+const props = defineProps({
+    code: {
+        type: String,
+        default: ''
+    }
+})
+
+const formData = ref({
+    code: props.code
+})
+const tableOption = reactive([
+    {
+        prop: 'name',
+        label: '费用名称',
+        width:'160'
+    },
+    {
+        prop: 'bsWhareaName',
+        label: '供应商',
+        width: '160'
+    },
+    {
+        prop: 'bsWhType',
+        label: '提单号',
+        width: '160'
+    },
+    {
+        prop: 'bsWhType',
+        label: '仓库区域',
+        width: '160'
+    },
+    {
+        prop: 'bsWhType',
+        label: '仓库名称',
+        width: '160'
+    },
+    {
+        prop: 'bsWhType',
+        label: '原始集装箱号',
+        width: '160'
+    },
+    {
+        prop: 'bsWhType',
+        label: '托盘数量',
+        width: '160'
+    },
+    {
+        prop: 'bsWhType',
+        label: '组片数量',
+        width: '160'
+    },
+    {
+        prop: 'bsWhType',
+        label: '入仓日期',
+        width: '160'
+    },
+    {
+        prop: 'bsWhType',
+        label: '出仓日期',
+        width: '160'
+    },
+    {
+        prop: 'bsWhType',
+        label: '费用生成时间',
+        width: '160'
+    },
+    {
+        prop: 'bsWhType',
+        label: '金额',
+        width: '160'
+    },
+    {
+        prop: 'bsWhType',
+        label: '币值',
+        width: '160'
+    },
+    {
+        prop: 'bsWhType',
+        label: '凭证',
+        width: '160'
+    },
+    {
+        prop: 'bsWhType',
+        label: '备注',
+        width: '160'
+    },
+    {
+        prop: 'bsWhType',
+        label: '状态',
+        width: '160'
+    }
+])
+
+// 搜索
+const tableRef = ref()
+const clickSearch = () => {
+    refresh()
+}
+const updateSearchData = (val) => {
+    formData.value = {
+        code: props.code
+    }
+    Object.assign(formData.value, val)
+}
+const resetForm = () => {
+    refresh()
+}
+
+
+// 刷新列表
+const refresh = () => {
+    tableRef.value.refresh()
+}
+
+
+
+
+</script>
+<style lang="scss" scoped>
+.table {
+    margin-top: .625rem;
+}
+
+.link {
+    margin-right: .625rem;
+}
+</style>
---
+++ src/views/WarehouseBilling/ExtraApproval/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <TableContent />
+    </div>
+</template>
+
+<script lang="ts" setup>
+import TableContent from './components/TableContent.vue'
+</script>
+<style lang="scss" scoped></style>
